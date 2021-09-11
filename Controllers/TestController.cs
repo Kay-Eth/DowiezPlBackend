ede4b100aed066b896fb2eca7e87c9719ef88947
Kay Eth <kayeth@outlook.com>
2021-09-11 16:39:21 +0200

    It's working

---
+++ Controllers/TestController.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DowiezPlBackend.Controllers
+{
+    /// <summary>
+    /// Controller for testing new features
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+
+        [HttpGet("Ping")]
+        public async Task<ActionResult> Ping()
+        {
+            return await Task.Run(() => Ok("PONG"));
+        }
+    }
+}
